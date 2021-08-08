6ea43e4a920040a294b7ec22b62f01eb9b76a1e1
jhansi <jmatluri@gmail.com>
2021-08-08 15:00:49 -0500

    added sysout line

--- src/main/java/com/qa/ninja/base/BasePage.java
+++ src/main/java/com/qa/ninja/base/BasePage.java
@@ -129,6 +129,7 @@ public class BasePage {
 		FileInputStream ip;
 		try {
 			ip = new FileInputStream(".\\src\\main\\java\\com\\qa\\ninja\\config\\config.properties");
+			System.out.println("hi");
 			try {
 				prop.load(ip);
 			} catch (IOException e) {
